6ac4cb2349db2ea0eb50822a8521e335f8cf458a
JereGuale <gualejeremi@gmail.com>
2025-10-18 15:05:00 -0500

    feat: develop_basededatos - cambios de base de datos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    // Agregado: plugin de Google Services para procesar google-services.json
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -45,4 +47,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Firebase BOM y módulos (Firestore + Auth)
+    implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
 }

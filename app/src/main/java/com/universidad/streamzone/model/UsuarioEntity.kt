dd51d2157e3f50e756fa10b092ce751c5b901b96
GonLuiZ <gonluiz.hispa@gmail.com>
2025-10-24 17:02:23 -0500

    feat: agregar campos de sincronización a UsuarioEntity"

--- app/src/main/java/com/universidad/streamzone/model/UsuarioEntity.kt
+++ app/src/main/java/com/universidad/streamzone/model/UsuarioEntity.kt
@@ -1,18 +1,21 @@
 package com.universidad.streamzone.model
-
-import android.widget.EditText
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "usuarios")
-
 data class UsuarioEntity(
     @PrimaryKey(autoGenerate = true)
-    val id: Int=0,
+    val id: Int = 0,
 
     val fullname: String,
     val email: String,
     val phone: String,
     val password: String,
-    val confirmPassword: String
+    val confirmPassword: String,
+
+    // campo para control de sincronizacion
+    val sincronizado: Boolean = false,
+
+    // ID de Firebase cuando se sincroniza
+    val firebaseId: String? = null
 )
